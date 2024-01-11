aba43e5b07e0f111a0ba3ad0b648beb0080f54f7
Felipe Vicente <vcnte.felipe@outlook.com>
2024-01-11 18:44:26 -0300

    feat: API project was configurated with connection string and secrets

---
+++ JwtLogin.Api/Program.cs
@@ -0,0 +1,10 @@
+﻿using JwtLogin.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddConfiguration();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
